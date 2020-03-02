79e778657d2b5805467377c9639aa8b5e60bf572
林一诚 <30539469+moin111@users.noreply.github.com>
2020-03-03 00:37:45 +0800

    moin111

--- include/db.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST','localhost');//数据库连接地址，默认：localhost或127.0.0.1
-define('DB_USER','root');//数据库账号
-define('DB_PASSWD','root');//数据库密码
-define('DB_NAME','user');//数据库名称
-define('DB_PRE','eruyi_');//数据库前缀
-?>
